090012b29fec515596f2fd629561b25961c29779
RdotSilva <RyandotSilva@gmail.com>
2021-04-25 13:29:55 -0400

    Add Project component mock

---
+++ src/Components/Project.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Row, Col } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import beaver from "../assets/beaver.png";
+
+const Project = ({ project }) => {
+  return (
+    <>
+      <Row className="projectContainer" justify="center">
+        <Col span={3}>
+          <img src={beaver}></img>
+        </Col>
+        <Col span={3}>
+          <div>Project Name</div>
+        </Col>
+        <Col span={3}>
+          <EditOutlined />
+        </Col>
+        <Col span={3}>Time Date</Col>
+        <Col span={3}>
+          <DeleteOutlined />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Project;
