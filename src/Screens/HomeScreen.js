f8d5159615d7b170088b40fa5e294fd7517544ea
RdotSilva <RyandotSilva@gmail.com>
2021-04-25 12:34:46 -0400

    Add TODO items

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -4,14 +4,19 @@ import beaver from "../assets/beaver.png";
 import plus from "../assets/plus.png";
 
 const HomeScreen = () => {
+  // TODO: Project State to track projects
+
   return (
     <Row className="headerContainer">
       <Row className="beaverIconContainer">
         <img className="beaverIcon" src={beaver}></img>
       </Row>
       <Col className="myProjectsText">My Projects</Col>
+      // TODO: Render Project Form onClick
       <img className="plusImage" src={plus}></img>
     </Row>
+
+    // TODO: Render Project List with a list of projects from state
   );
 };
 
