5ddfc089d85f1eef83bc4e1e5633c85350439cc3
RdotSilva <RyandotSilva@gmail.com>
2021-04-25 14:31:16 -0400

    Add key property

--- src/Components/ProjectList.jsx
+++ src/Components/ProjectList.jsx
@@ -6,7 +6,9 @@ import { Row } from "antd";
  * Responsible for rendering a list of projects
  */
 const ProjectList = ({ projectList }) => {
-  return projectList.map((project) => <Project project={project} />);
+  return projectList.map((project) => (
+    <Project key={project.id} project={project} />
+  ));
 };
 
 export default ProjectList;
