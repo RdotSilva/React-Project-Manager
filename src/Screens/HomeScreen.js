98ee386f4ab1cc9921a2e026b3c3c11b98a20c06
RdotSilva <RyandotSilva@gmail.com>
2021-04-25 12:32:06 -0400

    Add main design for project page

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Row, Col, Image, Divider } from "antd";
+import beaver from "../assets/beaver.png";
+import plus from "../assets/plus.png";
 
 const HomeScreen = () => {
-  return <div>Project Manager Home Screen</div>;
+  return (
+    <Row className="headerContainer">
+      <Row className="beaverIconContainer">
+        <img className="beaverIcon" src={beaver}></img>
+      </Row>
+      <Col className="myProjectsText">My Projects</Col>
+      <img className="plusImage" src={plus}></img>
+    </Row>
+  );
 };
 
 export default HomeScreen;
